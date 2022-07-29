ab360f3e47cddc0157cfa63eaa43617b7658b68c
hakanrsln <hakanrsln06@gmail.com>
2022-07-30 00:24:47 +0300

    Added Emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,23 @@ import './contact.css';
 import {MdOutlineEmail} from 'react-icons/md';
 import {RiMessengerLine} from 'react-icons/ri';
 import {BsWhatsapp} from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form =useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7e8szy9', 'template_or73eu1', form.current, 'If-vl0gy8JbNnkVet')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -30,7 +45,7 @@ const Contact = () => {
             <a href="https://wa.me/905347262282" target="_blank" rel="noreferrer">Send A Message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
             <input type="text" name='name' placeholder='Your Full Name' required />
             <input type="email" name='email' placeholder='Your Email' required />
             <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
