f11b1bcea06e677490ebf5cdfa72e3bf5ce725e7
hakanrsln <hakanrsln06@gmail.com>
2022-08-07 14:18:29 +0300

    type effet Used, testimonials removed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
      <Experience />
      <Services />
      <Portfolio />
-     <Testimonials />
+     {/**<Testimonials /> */}
      <Contact />
      <Footer />
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+
 import './header.css';
 import CTA from './CTA';
 import me from '../../assets/me2.png';
 import HeaderSocials from './HeaderSocials';
 
+
+
+
 const Header = () => {
+ 
+  
+
   return (
     
       <header id="#">
         <div className="container header__container">
           <h4 className='greeting'>Hello, I'm</h4>
-          <h1>Hakan ERARSLAN</h1>
-          <h4 className='text-light'>Web Developer</h4>
+         <h1 className='greeting-titles'> <Typewriter 
+            options={{
+              strings: ['Hakan Erarslan', 'a Web Developer', 'Team-Player'],
+              autoStart: true,
+              loop: true,
+            }}
+           />      </h1> 
+             {/** <h4 className='text-light'>Web Developer</h4> */}
           <CTA />
           <HeaderSocials />
            <div className='me'>
